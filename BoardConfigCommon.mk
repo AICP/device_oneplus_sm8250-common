a729348dd9cf8a40deffb5d7934633274ee123da
Lucas Dupin <dupin@google.com>
2020-03-14 15:01:44 -0700

    sm8250-common: Enable blurs, under dev-option
    Test: make
    Test: manual
    Bug: 149792636
    Change-Id: I607ce7de50545b87c3b4d98bb149355c84631e53

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -65,6 +65,7 @@ TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 
 # A/B
 AB_OTA_UPDATER := true
