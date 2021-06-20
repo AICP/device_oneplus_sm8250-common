1576b7aa0b2448ad943649de06a8c85d991e6f99
LuK1337 <priv.luk@gmail.com>
2021-06-20 13:00:22 +0800

    sm8250-common: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172

--- common.mk
+++ common.mk
@@ -305,6 +305,9 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
     libhwbinder.vendor
 
 # HotwordEnrollement app permissions
