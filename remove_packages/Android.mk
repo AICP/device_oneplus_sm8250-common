san9-s-dumpster/device_oneplus_sm8250-common-2

sm8250-common: Inherit OnePlus Camera packages
* https://gitlab.com/cjh1249131356/vendor_oneplus_opcamera

remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := NfcNci MusicFX SoundAmplifierPrebuilt
+LOCAL_OVERRIDES_PACKAGES := NfcNci MusicFX SoundAmplifierPrebuilt Snap Camera2
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null

AICP/device_oneplus_sm8250-common

Change-Id: I782d9878384939765f24226e96133e8aa917fee7

remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AudioFX NfcNci MusicFX
+LOCAL_OVERRIDES_PACKAGES := AudioFX NfcNci MusicFX Snap Camera2
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null