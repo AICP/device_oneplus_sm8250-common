1bc4422c3863aa9b559046edadf6534efd4e601a
kubersharma001 <kubersharma001@gmail.com>
2022-01-04 01:40:00 +0800

    sm8250-common: props: Set frame rate multiple threshold to 120
    Also fixes the Netflix 120hz stuck issue as mentioned
    on Change-Id: Icdd392332a6f44d5d1b2dc1e6e654b11f9e95da2
    
    test: Play a Netflix or YT video; it goes down to lowest refresh rate.
    
    Change-Id: Ie4d5c8c9a45079306ad51e1607ae72eb9493ee0c

--- common.mk
+++ common.mk
@@ -532,6 +532,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Surface Flinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    debug.sf.frame_rate_multiple_threshold=120 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.has_HDR_display=true \
     ro.surface_flinger.has_wide_color_display=true \
