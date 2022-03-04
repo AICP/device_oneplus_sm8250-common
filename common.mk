9dbfb4c0da5129d3a5ef8835e08596c45aabfad0
LuK1337 <priv.luk@gmail.com>
2022-03-04 21:41:06 +0300

    sm8250-common: Don't build MusicFX & AudioFX
    Change-Id: I0ff029e565f1c1becfa6d1e09bc6d8c4fa45039b

--- common.mk
+++ common.mk
@@ -420,6 +420,10 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
