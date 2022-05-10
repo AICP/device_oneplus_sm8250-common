7a1a3fa84001cd76a764c2c90fd9705b4f629d75
Michael Bestas <mkbestas@lineageos.org>
2022-05-11 02:48:48 +0300

    sm8250-common: Set ro.boot.hardware.revision based on RF version
    ro.boot.hardware.revision is used in fastbootd UI, getvar hw-revision
    output and Settings app.
    
    Set based on the RF version so external tools can use it to flash
    the correct firmware variant and show the HW variant in Settings.
    
    Change-Id: Ib92b080ace4d14fc2f13b5b3705249a15f1671c8

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -185,6 +185,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.oplus.hw.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.oplus.hw.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.factory.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
@@ -214,6 +222,15 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.oplus.hw.rc.recovery
+LOCAL_MODULE_STEM  := init.oplus.hw.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.oplus.hw.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
