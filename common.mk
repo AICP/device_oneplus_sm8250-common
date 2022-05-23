aospa-op8/android_device_oneplus_sm8250-common

sm8250-common: Enable Android Beam
Our NXP NFC supports this

Change-Id: I8a45ab5f8e9e3b0fdcc38452ca7ed34fb4821218

common.mk
@@ -413,6 +413,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/nxp/opensource/sn100x/halimpl/SN100x/halimpl/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
 
+# NFC (Android Beam)
+PRODUCT_COPY_FILES += \
+    vendor/kscope/config/permissions/android.software.nfc.beam.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.nfc.beam.xml
+
 # OMX
 PRODUCT_PACKAGES += \
     init.qti.media.sh \

AICP/device_oneplus_sm8250-common

common.mk
@@ -397,6 +397,10 @@ PRODUCT_PACKAGES += \
     SecureElement \
     Tag
 
+# NFC (Android Beam)
+PRODUCT_COPY_FILES += \
+    vendor/aicp/config/permissions/android.software.nfc.beam.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.nfc.beam.xml
+
 # OMX
 PRODUCT_PACKAGES += \
     init.qti.media.sh \