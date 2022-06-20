4eb476500bcbb4f8eaefd440acafdf4fa94d3203
Edwin Moquete <edwinmmoquete@gmail.com>
2022-06-19 18:04:55 -0700

    sm8250-common: rootdir: Kang runtime cpuset from crosshatch
    Change-Id: I286023ed61340901f994e9ab68f88b776aacc578

--- init/init.qcom.post_boot.sh
+++ init/init.qcom.post_boot.sh
@@ -5738,8 +5738,15 @@ case "$target" in
 	echo 400000000 > /proc/sys/kernel/sched_coloc_downmigrate_ns
 
 	# cpuset parameters
-	echo 0-3 > /dev/cpuset/background/cpus
+	#echo 0-3 > /dev/cpuset/background/cpus
+	#echo 0-3 > /dev/cpuset/system-background/cpus
+
+	#echo 0-6 > /dev/cpuset/foreground/cpus
+	echo 0-7 > /dev/cpuset/top-app/cpus
+	echo 0-5 > /dev/cpuset/foreground/cpus
+	echo 0-1 > /dev/cpuset/background/cpus
 	echo 0-3 > /dev/cpuset/system-background/cpus
+	echo 0-3 > /dev/cpuset/restricted/cpus
 
 	# Turn off scheduler boost at the end
 	echo 0 > /proc/sys/kernel/sched_boost
